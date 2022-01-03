9333b00f2b07095aa51756d0f66393d3b6d09ebb
ClausClaus <weixinla84@gmail.com>
2022-01-03 19:10:39 +0800

    format code

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,48 +1,58 @@
-import * as React from 'react';
+import * as React from 'react'
+import { ToastComponent, AlertComponent } from 'amis'
 import {
-    ToastComponent,
-    AlertComponent,
-} from 'amis';
-import { Route, Switch, Redirect, BrowserRouter as Router, HashRouter } from "react-router-dom";
-import { observer } from 'mobx-react';
-import { IMainStore } from '../stores';
-import Login from './Login';
-import Register from './Register';
-import AdminRoute from './admin/index';
-import NotFound from './404';
+  Route,
+  Switch,
+  Redirect,
+  BrowserRouter as Router,
+  HashRouter,
+} from 'react-router-dom'
+import { observer } from 'mobx-react'
+import { IMainStore } from '../stores'
+import Login from './Login'
+import Register from './Register'
+import AdminRoute from './admin/index'
+import NotFound from './404'
 
-import 'froala-editor/css/froala_style.min.css';
-import 'froala-editor/css/froala_editor.pkgd.min.css';
+import 'froala-editor/css/froala_style.min.css'
+import 'froala-editor/css/froala_editor.pkgd.min.css'
+import history from '../utils/history'
 
-let ContextPath = '';
+let ContextPath = ''
 
 // gh-pages 环境时
 if (process.env.NODE_ENV === 'production') {
-    ContextPath = '/amis-admin'
+  ContextPath = '/amis-admin'
 }
 
-export default observer(function({store}:{
-    store:IMainStore
-}) {
-    return (
-        <Router>
-            <div className="routes-wrapper">
-                <ToastComponent key="toast" position={'top-right'} theme={store.theme} />
-                <AlertComponent key="alert" theme={store.theme} />
-                <Switch>
-                    <Redirect to={`${ContextPath}/login`} from={`${ContextPath}/`} exact />
-                    <Route path={`${ContextPath}/login`} exact component={Login} />
-                    <Route path={`${ContextPath}/register`} exact component={Register} />
-                    
-                    {store.user.isAuthenticated ? (
-                        <Route path={`${ContextPath}/admin`} component={AdminRoute} />
-                    ) : (
-                        <Route path="*" exact component={Login} />
-                    )}
+export default observer(function ({ store }: { store: IMainStore }) {
+  return (
+    <Router>
+      <div className="routes-wrapper">
+        <ToastComponent
+          key="toast"
+          position={'top-right'}
+          theme={store.theme}
+        />
+        <AlertComponent key="alert" theme={store.theme} />
+        <Switch>
+          <Redirect
+            to={`${ContextPath}/login`}
+            from={`${ContextPath}/`}
+            exact
+          />
+          <Route path={`${ContextPath}/login`} exact component={Login} />
+          <Route path={`${ContextPath}/register`} exact component={Register} />
 
-                    <Route component={NotFound} />
-                </Switch>
-            </div>
-        </Router>
-    );
-});
+          {store.user.isAuthenticated ? (
+            <Route path={`${ContextPath}/admin`} component={AdminRoute} />
+          ) : (
+            <Route path="*" exact component={Login} />
+          )}
+
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
+  )
+})
--- utils/schema2component.tsx
+++ utils/schema2component.tsx
@@ -1,11 +1,11 @@
-import { Schema } from "amis/lib/types";
-import AMisRenderer from "../components/AMisRenderer";
-import React = require("react");
+import { Schema } from 'amis/lib/types'
+import AMisRenderer from '../components/AMisRenderer'
+import React = require('react')
 
-export default function(schema:Schema) {
-    return (props:any) => {
-        return (
-            <AMisRenderer schema={schema} {...props} />
-        )
-    }
+export default function (schema: Schema) {
+  return (props: any) => {
+    console.log(`props`, props)
+
+    return <AMisRenderer schema={schema} {...props} />
+  }
 }
