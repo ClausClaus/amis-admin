bf4a1912eb86b29377a12cf2d8c23f066fcb08c5
ClausClaus <weixinla84@gmail.com>
2022-01-05 19:21:09 +0800

    初始化页面相互通信的组件

--- routes/admin/form/Editor.tsx
+++ routes/admin/form/Editor.tsx
@@ -2,25 +2,16 @@ import schema2component from '../../../utils/schema2component'
 
 const schema = {
   type: 'page',
-  title: '代码编辑器',
-  subTitle:
-    '使用的monaco-editor,用到了 worker, 如果控制台没有报错，说明一切正常。',
+  name: 'editor',
+  title: 'B页面',
   body: [
     {
-      type: 'form',
-      body: [
-        {
-          type: 'editor',
-          name: 'js',
-          label: 'Javascript',
-          size: 'md',
-          value: `console.log("Hello world");`,
-        },
-        {
-          type: 'wrapper',
-          body: 'url id params is ${word}',
-        },
-      ],
+      type: 'wrapper',
+      body: 'editor',
+    },
+    {
+      label: '按钮',
+      type: 'button',
     },
   ],
 }
--- routes/admin/form/Wizard.tsx
+++ routes/admin/form/Wizard.tsx
@@ -2,105 +2,45 @@ import schema2component from '../../../utils/schema2component'
 
 const schema = {
   type: 'page',
-  title: '表单向导',
-  subTitle:
-    '可以通过表单向导，将一个超长的表单页面拆分成多个步骤，一步一步指引用户完成。',
+  title: 'A页面',
+  name: 'wizard',
   body: [
     {
-      type: 'wizard',
-      actionFinishLabel: '确认',
-      api: '/api/saveWizard',
-      steps: [
-        {
-          title: '填写活动信息',
-          body: [
-            {
-              type: 'text',
-              name: 'title',
-              label: '活动标题',
-              required: true,
-              size: 'md',
-            },
-
-            {
-              type: 'date',
-              name: 'date',
-              label: '举办时间',
-              size: 'md',
-            },
-
-            {
-              type: 'input-number',
-              name: 'num',
-              label: '参与人数',
-              value: 10,
-              size: 'md',
-            },
-          ],
+      type: 'form',
+      mode: 'horizontal',
+      title: '',
+      api: {
+        url: '/api/form/save',
+        requestAdaptor: (api: any) => {
+          console.log('api :>> ', api)
+          return api
         },
-
+      },
+      // redirect: '/admin/form/basic/editor',
+      actions: [
         {
-          title: '填写赞助商信息',
-          body: [
-            {
-              type: 'text',
-              name: 'company',
-              label: '公司名称',
-              required: true,
-              size: 'md',
-            },
-
-            {
-              type: 'text',
-              name: 'money',
-              label: '赞助金额',
-              addOn: {
-                type: 'text',
-                label: '￥',
-              },
-              size: 'md',
-            },
-          ],
-        },
-
-        {
-          title: '确认',
-          mode: 'horizontal',
-          horizontal: {
-            leftFixed: 'sm',
+          label: '保存',
+          type: 'submit',
+          level: 'success',
+          onClick: (e: any, props: any) => {
+            console.log('props :>> ', props)
+            console.log('event :>> ', e)
+            console.log('props :>> ', props.env)
           },
-          body: [
-            {
-              type: 'static',
-              name: 'company',
-              label: '活动标题',
-              labelClassName: 'text-muted',
-            },
-            {
-              type: 'static-date',
-              name: 'date',
-              label: '举办时间',
-              labelClassName: 'text-muted',
-            },
-            {
-              type: 'static',
-              name: 'num',
-              label: '参与人数',
-              labelClassName: 'text-muted',
-            },
-            {
-              type: 'static',
-              name: 'company',
-              label: '公司名称',
-              labelClassName: 'text-muted',
-            },
-            {
-              type: 'static',
-              name: 'money',
-              label: '赞助金额',
-              labelClassName: 'text-muted',
-            },
-          ],
+        },
+      ],
+      body: [
+        {
+          label: '名字',
+          type: 'input-text',
+          name: 'name',
+          value: 'Aaron',
+        },
+        {
+          label: '年龄',
+          type: 'input-number',
+          name: 'age',
+          value: 10,
         },
       ],
     },
